025faeb59926e00f9828b9442efcef5264d6ea21
AmirAli <amiralidev20@gmail.com>
2022-10-12 22:22:12 +0330

    psr-4-autoloader-is-ok

---
+++ App/Services/Payment/OnlinePayment.php
@@ -0,0 +1,6 @@
+<?php namespace app\Services\Payment;
+
+class OnlinePayment
+{
+
+}
--- index.php
+++ index.php
@@ -4,4 +4,9 @@
 
 //echo "Front Controller";
 
-echo $_SERVER['REQUEST_URI'];
+//echo $_SERVER['REQUEST_URI'];
+
+
+require "vendor/autoload.php";
+
+new \app\Core\Request();
