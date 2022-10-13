ca85fc625d2c27c127754eb1013681b50346c462
AmirAli <amiralidev20@gmail.com>
2022-10-13 13:38:11 +0330

    Update-Router

--- App/Core/StupidRouter.php
+++ App/Core/StupidRouter.php
@@ -1,11 +1,37 @@
 <?php namespace app\Core;
 
+use app\Utilities\Url;
+
 class StupidRouter
 {
     private $routes;
 
     public function __construct()
     {
-        $this->routes = [];
+        $this->routes = [
+            '/colors/blue' => 'Colors/blue.php',
+            '/colors/red' => 'Colors/red.php',
+            '/colors/green' => 'Colors/green.php'
+        ];
     }
+
+    public function run()
+    {
+        $current_route = Url::current_route();
+        foreach ($this->routes as $route => $view) {
+            if ($current_route == $route) {
+                $this->includeAndDie(BASE_PATH . "views/$view");
+            }
+        }
+        # 404 Header
+        header("HTTP/1.0 404 Not Found");
+        $this->includeAndDie(BASE_PATH . "views/errors/404.php");
+    }
+
+    private function includeAndDie($viewPath)
+    {
+        include $viewPath;
+        die;
+    }
+
 }
--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 
+use app\Core\StupidRouter;
+
 # Front Controller
 include "bootstrap/init.php";
 
-
-$route = \app\Utilities\Url::current_route();
-if ($route == "/colors/blue")
-    include BASE_PATH . "views/Colors/blue.php";
-
-
-if ($route == "/colors/red")
-    include BASE_PATH . "views/Colors/red.php";
-
-
-if ($route == "/colors/green")
-    include BASE_PATH . "views/Colors/green.php";
+$router = new StupidRouter();
+$router->run();
